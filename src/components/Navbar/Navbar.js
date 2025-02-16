9244f4aadd7695a00985ff27dd5d032113cef6ea
shreya <shreyakhadka007@gmail.com>
2025-02-16 15:55:49 +0545

    third commit changes in navbar final

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import styles from "./Navbar.css";
+import styles from "./Navbar.module.css";
+import { FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
   const location = useLocation();
@@ -10,7 +11,9 @@ const Navbar = () => {
       <Link to="/home" className={location.pathname === "/home" ? styles.active : ""}>Home</Link>
       <Link to="/request-blood" className={location.pathname === "/request-blood" ? styles.active : ""}>Request Blood</Link>
       <Link to="/blood-stock" className={location.pathname === "/blood-stock" ? styles.active : ""}>Real-time Blood Stock</Link>
-      <Link to="/search" className={location.pathname === "/search" ? styles.active : ""}>Search</Link>
+      <Link to="/search" className={location.pathname === "/search" ? styles.active : ""}> 
+      <FaSearch className="search-icon" />Search
+      </Link>
       <Link to="/profile" className={location.pathname === "/profile" ? styles.active : ""}>Profile</Link>
     </nav>
   );
